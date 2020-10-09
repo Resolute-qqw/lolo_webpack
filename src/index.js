20dc553be6f0cdf78961b0047111e4d588164319
李浩淇(EX-LIHAOQI001) <1425679428@qq.com>
2020-10-09 15:41:32 +0800

    图片 css 处理

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@
 let Header = require('./header')
 let Sidebar = require('./sidebar')
 let Content = require('./content')
+import Logo1 from './assets/img/logo.png'
+import './style/logo.css'
+let img = new Image()
+img.src = Logo1
+img.style.background = '#ccc'
+img.classList.add('logo')
+let root = document.getElementById('root')
+root.append(img)
 
 new Header()
 new Sidebar()
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,28 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        // placeholders
+                        name: '[name]_hash.[ext]',
+                        outputPath: 'img/',
+                        limit: 2048,
+                    },
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ]
+    },
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve('dist')
     }
 }
